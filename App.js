3211dbfcbc184d5949b82082b3d3e148f5a0204c
atofo <atofito@hotmail.com>
2023-09-08 23:27:28 -0700

    judios 3.0

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Guten Abend, bis samstag</Text>
+      <Text>Amo a los judios :D</Text>
       <StatusBar style="auto" />
     </View>
   );
